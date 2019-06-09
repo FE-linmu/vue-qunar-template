f56b2f24c605f5f7f91303a81ee99ef2e0c3cf70
979623625@qq.com <979623625@qq.com>
2019-06-09 23:08:28 +0800

    '首页的header的开发，icon的使用，stylus的使用'

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,13 +1,17 @@
 /* 展示模板 */
 <template>
   <div id='home'>
-    Home
+    <home-header></home-header>
   </div>
 </template>
 <script>
 // 导入组件
+import HomeHeader from './components/Header'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components:{
+    HomeHeader
+  }
 }
 </script>
 <style scope>
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,62 @@
+/* 展示模板 */
+<template>
+  <div class='header'>
+    <div class='header-left'>
+      <span class='iconfont back-icon'>
+        &#xe624;
+      </span>
+    </div>
+    <div class="header-input">
+      <span class='iconfont'>&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">城市
+      <span class='iconfont arrow-icon'>&#xe64a;</span>
+    </div>
+  </div>
+</template>
+<script>
+//导入组件
+export default {
+  name: 'HomeHeader'
+}
+</script>
+<style lang="stylus" scoped>
+@import '~styles/varibies.styl';
+.header {
+  line-height: 0.86rem;
+  display: flex;
+  background:$bgColor;
+  color: #fff;
+
+  .header-left {
+    width: 0.64rem;
+    margin-left:.2rem;
+    .back-icon {
+      text-align: center;
+      font-size: 0.4rem;
+    }
+  }
+
+  .header-input {
+    flex: 1;
+    background: #fff;
+    height: 0.64rem;
+    line-height: 0.64rem;
+    margin-top: 0.12rem;
+    margin-left: 0.2rem;
+    padding-left: 0.2rem;
+    border-radius: 0.02rem;
+    color: #ccc;
+  }
+
+  .header-right {
+    width: 1.24rem;
+    text-align: center;
+
+    .arrow-icon {
+      font-size: 0.2rem;
+    }
+  }
+}
+</style>
