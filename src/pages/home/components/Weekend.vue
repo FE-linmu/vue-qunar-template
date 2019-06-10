ea16aae1bf8e5ef0f3a6a541369abfcb1f72a567
979623625@qq.com <979623625@qq.com>
2019-06-10 11:44:23 +0800

    首页周末去哪玩

---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,88 @@
+/* 展示模板 */
+<template>
+  <div>
+    <div class='recommend-title'>
+      周末去哪儿
+    </div>
+    <ul>
+      <li class='item border-bottom'
+          v-for="item of recommendList"
+          :key="item">
+        <div class='item-img-wrapper'>
+          <img class='item-img'
+               :src="item.imgUrl"
+               alt="">
+        </div>
+        <div class='item-info'>
+          <p class='item-title'>{{item.title}}</p>
+          <p class='item-desc'>{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+// 导入组件
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1811/52/b6776b49d27025.jpg_r_640x214_35ae205c.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫大连首站，浪漫的海洋主题乐园'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1811/52/b6776b49d27025.jpg_r_640x214_35ae205c.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫大连首站，浪漫的海洋主题乐园'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1811/52/b6776b49d27025.jpg_r_640x214_35ae205c.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫大连首站，浪漫的海洋主题乐园'
+      }]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl';
+
+.recommend-title {
+  line-height: 0.8rem;
+  background: #eee;
+  text-indent: 0.2rem;
+  margin-top: 0.2rem;
+}
+
+.item-img-wrapper {
+  height: 0;
+  overflow: hidden;
+  padding-bottom: 33.9%;
+
+  .item-img {
+    width:100%;
+    padding: 0.1rem;
+  }
+}
+
+.item-info {
+  flex: 1;
+  padding: 0.1rem;
+  min-width: 0;
+
+  .item-title {
+    line-height: 0.54rem;
+    font-size: 0.32rem;
+    ellipsis();
+  }
+
+  .item-desc {
+    line-height: 0.4rem;
+    color: #ccc;
+    ellipsis();
+  }
+}
+</style>
