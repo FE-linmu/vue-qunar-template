532d01813cae114a3ac0853528e4e844fc5ff95b
979623625@qq.com <979623625@qq.com>
2019-06-12 13:36:20 +0800

    'city-search'

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,15 +2,18 @@
 <template>
   <div class='city'>
     <city-header></city-header>
+    <city-search></city-search>
   </div>
 </template>
 <script>
 //导入组件
 import CityHeader from './components/Header'
+import CitySearch from './components/Search'
 export default {
   name: 'City',
   components: {
-    CityHeader
+    CityHeader,
+    CitySearch
   }
 }
 </script>
---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,34 @@
+/* 展示模板 */
+<template>
+  <div class='search'>
+    <input type="text"
+           placeholder="输入城市名或拼音"
+           class='search-input'>
+  </div>
+</template>
+<script>
+//导入组件
+export default {
+  name: 'CitySearch'
+}
+</script>
+<style lang="stylus" scoped>
+@import '~styles/varibies.styl';
+
+.search {
+  height: 0.72rem;
+  padding: 0 0.1rem;
+  background: $bgColor;
+
+  .search-input {
+    width: 100%;
+    height: 0.62rem;
+    line-height: 0.62rem;
+    padding: 0 0.1rem;
+    box-sizing: border-box;
+    text-align: center;
+    border-radius: 0.06rem;
+    color: #666;
+  }
+}
+</style>
